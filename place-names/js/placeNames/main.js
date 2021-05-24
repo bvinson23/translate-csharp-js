2d78a820acea4d9113561811ab601d035577ef04
bvinson23 <brandonvinson23@gmail.com>
2021-05-24 11:37:30 -0500

    PlaceNames complete

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -3,4 +3,19 @@
 const names = [
     "Nashville", "Hong Kong", "The back yard", "Earth",
     "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
-]
+];
+
+console.log("All Place Names");
+
+for (placeName of names) {
+    console.log(placeName)
+}
+
+console.log("\n");
+
+const theNames = names.filter(n => n.startsWith("The"));
+
+console.log("'The' Place Names");
+for (placeName of theNames) {
+    console.log(placeName);
+}
