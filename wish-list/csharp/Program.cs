d7cbef4418ef87ce8149de51fad90c7d792b8b54
bvinson23 <brandonvinson23@gmail.com>
2021-05-26 15:40:33 -0500

    wish complete

--- wish-list/csharp/Program.cs
+++ wish-list/csharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace csharp
 {
@@ -6,7 +7,33 @@ namespace csharp
     {
         static void Main(string[] args)
         {
+           List<Wish> iWishFor = new List<Wish>
+           {
+               new Wish() {
+                   WishName = "An original eight track of William Shatner's 'Lucy in the Sky with Diamonds'"
+               },
+               new Wish() {
+                   WishName = "The ability to think of interesting wishes on short notice"
+               },
+               new Wish() {
+                   WishName = "A washtub of Skittles"
+               },
+               new Wish() {
+                   WishName = "World peace, but not the kind of world peace where all the humans are removed from existencee"
+               },
+               new Wish() {
+                   WishName = "A TV that is NOT internet-capable"
+               },
+               new Wish() {
+                   WishName = "The ability to fly, but, like, really fast...not just, like, how fast I can walk, but, like, fast, you know?"
+               }
+           };
+
            Console.WriteLine("My wishes...");
+           foreach(Wish wish in iWishFor)
+           {
+               Console.WriteLine($"{wish.WishName}");
+           }
         }
     }
 }
---
+++ wish-list/csharp/Wish.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace csharp
+{
+    public class Wish
+    {
+        public string WishName { get; set; }
+    }
+}
